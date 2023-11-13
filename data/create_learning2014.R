a27e42699aab31543ad4510c714d747bd86bc121
kattilakoski <matilda.kattilakoski@gmail.com>
2023-11-13 18:46:08 +0200

    committing the assignment 2 rmd file

---
+++ data/create_learning2014.R
@@ -0,0 +1,62 @@
+# date: 10.11.2023
+# author: Matilda Kattilakoski
+# description: Data wrangling part of assignment 2 on the IODS2023 course.
+
+# read the data into memory
+lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+
+# Look at the dimensions of the data
+dim(lrn14)
+
+# Look at the structure of the data
+str(lrn14)
+
+
+#Create analysis dataset with the variables gender, age, attitude, deep, stra, surf and points
+
+# questions related to deep, surface and strategic learning
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+# select the columns related to deep learning 
+deep_columns <- select(lrn14, one_of(deep_questions))
+# and create column 'deep' by averaging
+lrn14$deep <- rowMeans(deep_columns)
+
+# select the columns related to surface learning 
+surface_columns <- select(lrn14, one_of(surface_questions))
+# and create column 'surf' by averaging
+lrn14$surf <- rowMeans(surface_columns)
+
+# select the columns related to strategic learning 
+strategic_columns <- select(lrn14, one_of(strategic_questions))
+# and create column 'stra' by averaging
+lrn14$stra <- rowMeans(strategic_columns)
+
+#Create analysis dataset with the variables gender, age, attitude, deep, stra, surf and points(tarkista isot kirjaimet kun ajat tän maanantaina)
+keep_columns <- c("gender","Age","attitude", "deep", "stra", "surf", "Points")
+
+# select the 'keep_columns' to create a new dataset
+learning2014 <- select(lrn14, one_of(keep_columns))
+
+#TArviiko tätä? Scale all combination variables to the original scales (by taking the mean).
+# divide each number in the column vector
+#lrn14$Attitude / 10
+
+# create column 'attitude' by scaling the column "Attitude"
+#lrn14$attitude <- lrn14$Attitude / 10
+
+# select rows where points is greater than zero, tarkista onko points vai Points
+learning2014 <- filter(learning2014, Points > 0)
+
+#set woeking directory to IODS-project folder:
+setwd("C:/Users/.../.../.../IODS2023/IODS-project")
+
+write_csv(learning2014, "C:/Users/03232430/OneDrive - Valtion/Tohtorikurssit/IODS2023/IODS-project/data/learning2014.csv")
+learning2014 <- read_csv("C:/Users/03232430/OneDrive - Valtion/Tohtorikurssit/IODS2023/IODS-project/data/learning2014.csv")
+str(learning2014)
+head(learning2014)
+
+
+#Analysis part of the assignment 2:
