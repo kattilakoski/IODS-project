8baebfbc9340e315296d6fdf379128778d38a38d
kattilakoski <matilda.kattilakoski@gmail.com>
2023-11-13 18:54:30 +0200

    committing data wrangling part of assignment 2

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -53,8 +53,8 @@ learning2014 <- filter(learning2014, Points > 0)
 #set woeking directory to IODS-project folder:
 setwd("C:/Users/.../.../.../IODS2023/IODS-project")
 
-write_csv(learning2014, "C:/Users/03232430/OneDrive - Valtion/Tohtorikurssit/IODS2023/IODS-project/data/learning2014.csv")
-learning2014 <- read_csv("C:/Users/03232430/OneDrive - Valtion/Tohtorikurssit/IODS2023/IODS-project/data/learning2014.csv")
+write_csv(learning2014, "C:/Users/Tohtorikurssit/IODS2023/IODS-project/data/learning2014.csv")
+learning2014 <- read_csv("C:/Users/Tohtorikurssit/IODS2023/IODS-project/data/learning2014.csv")
 str(learning2014)
 head(learning2014)
 
